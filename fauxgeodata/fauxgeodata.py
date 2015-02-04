0d4cb8f4ed88c5d75a4b90f028724163668fc9e6
Will <wbierbower@gmail.com>
2015-02-03 17:10:04 -0800

    Update fauxgeodata.py

--- fauxgeodata/fauxgeodata.py
+++ fauxgeodata/fauxgeodata.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+try:
+	from osgeo import gdal
+	from osgeo import ogr
+except:
+	import gdal
+	import ogr
 
 import numpy as np
 
@@ -13,3 +19,35 @@ def fauxgeodata():
 		hello_world (string): "hello world"!
 	'''
 	return "hello world"
+
+
+def generate_array():
+	pass
+
+def generate_geometries():
+	pass
+
+def create_raster(array):
+	'''
+	Creates an arbitrary raster
+	
+	Args:
+		array (np.array): raster values
+	
+	Returns:
+		None
+	'''
+	pass
+	
+
+def create_vector(geometries):
+	'''
+	Creates an arbitrary vector
+	
+	Args:
+		geometries (list): list of shapely geometries
+		
+	Returns:
+		None
+	'''
+	pass
