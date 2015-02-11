f5912b1bdc3324eaf17c68dd8daad440f3ca24b0
Will Bierbower <will@ncp-lila>
2015-02-11 11:48:54 -0800

    Starting to write create_raster function

--- fauxgeodata/fauxgeodata.py
+++ fauxgeodata/fauxgeodata.py
@@ -1,53 +1,77 @@
 # -*- coding: utf-8 -*-
 try:
-	from osgeo import gdal
-	from osgeo import ogr
+    from osgeo import gdal
+    from osgeo import ogr
+    from osgeo import osr
 except:
-	import gdal
-	import ogr
+    import gdal
+    import ogr
+    import osr
 
 import numpy as np
 
+
 def fauxgeodata():
-	'''
-	About
+    '''
+    About
 
-	Args:
-		None
+    Args:
+        None
 
-	Returns
-		hello_world (string): "hello world"!
-	'''
-	return "hello world"
+    Returns
+        hello_world (string): "hello world"!
+    '''
+    return "hello world"
 
 
 def generate_array():
-	pass
+    pass
+
 
 def generate_geometries():
-	pass
+    pass
 
-def create_raster(array):
-	'''
-	Creates an arbitrary raster
-	
-	Args:
-		array (np.array): raster values
-	
-	Returns:
-		None
-	'''
-	pass
-	
+
+def create_raster(filepath, orgX, orgY, pixWidth, pixHeight, array, proj=4326):
+    '''
+    Creates an arbitrary raster
+
+    Args:
+        filepath (string): where to save file
+        proj (int): EPSG code
+        orgX (float): bottom left x dimension?
+        array (np.array): raster values
+
+    Returns:
+        None
+    '''
+    num_bands = 1
+    rotX = 0
+    rotY = 0
+
+    rows = array.shape[0]
+    cols = array.shape[1]
+
+    driver = gdal.GetDriverByName('GTiff')
+    raster = driver.Create(filepath, cols, rows, num_bands, gdal.GDT_Byte)
+    raster.SetGeoTransform((orgX, pixWidth, rotX, orgY, rotY, pixHeight))
+
+    band = raster.GetRasterBand(1)  # Get only raster band
+    band.WriteArray(array)
+    raster_srs = osr.SpatialReference()
+    raster_srs.ImportFromEPSG(proj)
+    raster.SetProjection(raster_srs.ExportToWkt())
+    band.FlushCache()
+
 
 def create_vector(geometries):
-	'''
-	Creates an arbitrary vector
-	
-	Args:
-		geometries (list): list of shapely geometries
-		
-	Returns:
-		None
-	'''
-	pass
+    '''
+    Creates an arbitrary vector
+
+    Args:
+        geometries (list): list of shapely geometries
+
+    Returns:
+        None
+    '''
+    pass
