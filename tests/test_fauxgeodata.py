a6cd156c23b0704cf3a9008fd2ad43e7c02f3e91
Will Bierbower <will@ncp-lila>
2015-02-11 16:02:58 -0800

    adding a couple test cases

--- tests/test_fauxgeodata.py
+++ tests/test_fauxgeodata.py
@@ -9,6 +9,11 @@ Tests for `fauxgeodata` module.
 """
 
 import unittest
+import os
+
+import tempfile
+import numpy as np
+import rasterio as rio
 
 from fauxgeodata import fauxgeodata
 
@@ -18,9 +23,59 @@ class TestFauxgeodata(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_fauxgeodata(self):
-        guess = fauxgeodata.fauxgeodata()
-        self.assertEqual(guess, "hello world")
+    def test_fauxgeodata_1(self):
+        subdir = tempfile.mkdtemp("subdir")
+        filepath = os.path.join(subdir, 'test.tif')
+        orgX = 20
+        orgY = 20
+        pixWidth = 1
+        pixHeight = 1
+        rasterWidth = 10
+        rasterHeight = 20
+
+        array = np.random.randint(10, size=[rasterHeight, rasterWidth])
+        fauxgeodata.create_raster(
+            filepath,
+            orgX,
+            orgY,
+            pixWidth,
+            pixHeight,
+            array)
+
+        with rio.open(filepath, 'r') as rast:
+            self.assertEqual(rast.width, rasterWidth)
+            self.assertEqual(rast.height, rasterHeight)
+            self.assertEqual((rasterHeight, rasterWidth), rast.shape)
+            # rast.crs
+            # rast.affine
+
+    def test_fauxgeodata_2(self):
+        subdir = tempfile.mkdtemp("subdir")
+        filepath = os.path.join(subdir, 'test.tif')
+        orgX = 20
+        orgY = 20
+        pixWidth = 10000
+        pixHeight = 10000
+        rasterWidth = 100
+        rasterHeight = 800
+        proj = 26918
+
+        array = np.random.randint(10, size=[rasterHeight, rasterWidth])
+        fauxgeodata.create_raster(
+            filepath,
+            orgX,
+            orgY,
+            pixWidth,
+            pixHeight,
+            array,
+            proj=proj)
+
+        with rio.open(filepath, 'r') as rast:
+            self.assertEqual(rast.width, rasterWidth)
+            self.assertEqual(rast.height, rasterHeight)
+            self.assertEqual((rasterHeight, rasterWidth), rast.shape)
+            # rast.crs
+            # rast.affine
 
     def tearDown(self):
         pass
