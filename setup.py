e88cf8b4c815154a21ae209a0bc47f86ba6f696b
Will Bierbower <will@ncp-lila>
2015-02-11 16:53:20 -0800

    updating travis file 8

--- setup.py
+++ setup.py
@@ -33,7 +33,12 @@ setup(
     author='Will B',
     author_email='wbierbower@gmail.com',
     url='https://github.com/wbierbower/fauxgeodata',
-    packages=find_packages(),
+    packages=[
+        'fauxgeodata',
+        'tests'
+    ],
+    package_dir={'fauxgeodata': 'fauxgeodata',
+                 'tests': 'tests'},
     include_package_data=True,
     install_requires=requirements,
     license="BSD",
@@ -47,9 +52,6 @@ setup(
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
     ],
     test_suite='tests',
     tests_require=test_requirements
