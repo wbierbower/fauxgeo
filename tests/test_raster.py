a46c9e980859b2b7bb6405854563d000ca494467
Will Bierbower <will@Wills-MacBook-Pro-2.local>
2015-03-14 23:46:32 -0700

    working on Raster class

---
+++ tests/test_raster.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_raster
+----------------------------------
+
+Tests for Raster class.
+"""
+
+import unittest
+import os
+
+import tempfile
+import numpy as np
+
+from fauxgeodata import Raster
+
+
+class TestRaster(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_raster_1(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
